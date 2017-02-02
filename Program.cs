3d54e6675c472e7dd84780d207b07d140b13752c
clbokea <clbo@kea.dk>
2017-02-02 18:34:15 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+                host.Run();
+
+
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+       public void Configure(IApplicationBuilder app)
+       {
+           app.UseStaticFiles();
+
+           app.Run(
+               context => {return context.Response.WriteAsync("Hello from the web");}
+           );
+ 
+       }
+    }
+}
